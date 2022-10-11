54a976b68327415b0957e61a692d83981b235a5e
giobruno <giobruno@me.com>
2022-10-11 19:36:27 +0200

    done04

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,20 +3,20 @@ import React from "react";
 const age = 22
 
 
-
 export class Welcome extends React.Component {
     render() {
     return <div>
          <p>Welcome {this.props.name} </p> 
-           {age > 18 && age < 65 && this.props.name === 'John' && <p> {age} </p>}
+           {age > 18 && age < 65 && 
+           <p> <Age/> </p>}
          
          </div>;
     }
 
 }
 
-// class Age  extends React.Component {
-//     render(){
-//         return <p>Is your age {this.props.age} ?</p>
-//     }
-// }
+class Age  extends React.Component {
+    render(){
+        return <p> Your age is {this.props.age}</p>
+    }
+}
