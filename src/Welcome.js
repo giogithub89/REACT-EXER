64847ebf490d6ed9eefa657647d0435356af5734
giobruno <giobruno@me.com>
2022-10-11 20:12:08 +0200

    done05

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,7 +5,7 @@ export class Welcome extends React.Component {
     render() {
     return <div>
          <p>Welcome {this.props.name} </p> 
-           <p> <Age age = {22}/> </p>
+           <p> <Age age = {12}/> </p>
          
          </div>;
     }
@@ -17,9 +17,9 @@ class Age  extends React.Component {
     render(){
         return <div>
             
-            {this.props.age > 18 && this.props.age < 65  &&  
-            this.props.name === 'John' &&
-            <p> Your age is {this.props.age} </p>}
+            {this.props.age > 18   
+           ? <p> Your age is {this.props.age} </p>
+           : <p> You are very young </p> }
         </div>
     }
 }
