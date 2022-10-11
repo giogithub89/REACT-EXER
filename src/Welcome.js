3c0e4ea3f5c1165fccbe93e309f6322aea4d4f0d
giobruno <giobruno@me.com>
2022-10-11 18:19:26 +0200

    done cond 01

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,21 +1,21 @@
 import React from "react";
 
+const age = 22
 
 
 export class Welcome extends React.Component {
     render() {
     return <div>
-         <p>Welcome, {this.props.name} </p> 
-         <Age age="22"/>
-         
+         <p>Welcome {this.props.name} </p> 
+           {age > 18 && <p> {age} </p>}
          
          </div>;
     }
 
 }
 
-class Age  extends React.Component {
-    render(){
-        return <p>Is your age {this.props.age} ?</p>
-    }
-}
+// class Age  extends React.Component {
+//     render(){
+//         return <p>Is your age {this.props.age} ?</p>
+//     }
+// }
