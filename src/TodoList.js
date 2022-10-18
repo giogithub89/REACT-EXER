09dd7ca98f1ecbe5122e7045c6e7d18d0ebaaf21
giobruno <giobruno@me.com>
2022-10-18 09:29:47 +0200

    try

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,40 +4,23 @@ export class TodoList extends React.Component {
   state = {
     todos: [],
   };
-  // inputList = (event) => {
-  //   const value = event.target.task;
-  //   this.setState(() => {
-  //     return {
-  //       task: value,
-  //     };
-  //   });
-  //   console.log(value);
-  // };
+
   handleAddTask = (event) => {
     event.preventDefault();
     const todo = event.target.elements.todo.value;
     //update the state input field
-    //this.setState({ todos: [...this.state.todos, todo] });
     this.setState((state) => {
 			return {
 				todos: [...state.todos, todo],
+        todo:''
 			}
 		})
-    // this.state.tasks.push(this.state.newTask);
   };
+  
 
   render() {
-    //const elements = this.props.tasks.map((item) => <li>{item}</li>);
     return (
       <div>
-        {/* <ul>
-          {elements}
-          <input
-            name="inputList"
-            value={this.state.task}onChange={this.inputList}/>
-
-          <button onClick={this.handleAddTask}>Add Task</button>
-        </ul> */}
 
       <form onSubmit={this.handleAddTask}>
         <input data-testid="todo-input" name='todo'></input>
@@ -45,6 +28,7 @@ export class TodoList extends React.Component {
         
       </form>
       <ul>
+        
         {this.state.todos.map((todo, index) => <li key={index}>{todo}</li>)}
       </ul>
 
