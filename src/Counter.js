ab27b8099d6948c618a137f0cebff7baace21c63
giobruno <giobruno@me.com>
2022-10-22 17:18:46 +0200

    try counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,11 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function Counter(initialValue) {
-    const [counter, setCounter] = useState(0)
+export default function Counter(props, {initialValue = 0}) {
+    const [counter, setCounter] = useState(initialValue)
+
+    useEffect(()=> {
+      //props.onCounterChange(counter)
+      console.log(`current value is ${counter}`)
+    } , [counter, props])
+      
+    
 
     function increment(){
         setCounter(counter => counter + 1)
     }
+    
 
   return (
     <div>
