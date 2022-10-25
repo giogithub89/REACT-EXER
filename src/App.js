be9e7909f3dbbf424d4985b1987a4babb5ea5db1
giobruno <giobruno@me.com>
2022-10-25 10:17:35 +0200

    try

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Welcome from './Welcome';
 import React from 'react';
-import Counter from './Counter';
+
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Welcome name={'Giovanni'}/>} />
-      <Route path='/counter' element= {<Counter/> }/>
+      
     </Routes>
   );
 }
