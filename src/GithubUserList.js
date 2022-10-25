e6137bfaa1c060e41768f34b9449877619cf2a8a
giobruno <giobruno@me.com>
2022-10-25 16:09:25 +0200

    try

---
+++ src/GithubUserList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import GithubUser from './GithubUser'
+
+function GithubUserList() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default GithubUserList
