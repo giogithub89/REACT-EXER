1fae1b551d6b25bc1c5794dc8f7bb9bc757e14f4
giobruno <giobruno@me.com>
2022-10-25 19:30:03 +0200

    add error

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -2,22 +2,26 @@ import React, { useEffect, useState } from 'react'
 
 function GithubUser({username}) {
     const [data, setData] = useState(null)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
         fetch(`https://api.github.com/users/${username}`)
         .then(respone =>{
+            if(respone !== 200){
+                setError(new Error)
+            }
             return respone.json()
         })
         .then(json => {
             console.log(json)
             setData(json)
-        })
+        }).catch(error)
     }, [username] )
 
 
   return (
     <div>
-
+        {error && <h1>{error.message}</h1>}
         {data && <h1>{data.name}</h1>}
         <p>Followers: {data.followers}</p>
         <p>Repos: {data.public_repos}</p>
