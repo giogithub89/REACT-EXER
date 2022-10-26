f4b4bd647811cd510aa3e587cd0440d7857bdd33
giobruno <giobruno@me.com>
2022-10-26 11:37:14 +0200

    try

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Routes, Route, Link} from 'react-router-dom';
 import './App.css';
 import Welcome from './Welcome';
 import React from 'react';
@@ -8,10 +8,21 @@ import ShowGithubUser  from './ShowGithubUser';
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Welcome name={'Giovanni'}/>} />
-      <Route path='/counter' element= {<Counter/> }/>
-      <Route path="users/:username" element={<ShowGithubUser/>} />
+      <Route path="/" element={<Welcome name={"Giovanni"} />} />
+      <Route path="/counter" element={<Counter />} />
+      <Route path="users/:username" element={<ShowGithubUser />} />
 
+      <ul>
+        <li>
+          <Link to="/">Welcome</Link>
+        </li>
+        <li>
+          <Link to="/counter">Counter</Link>
+        </li>
+        <li>
+          <Link to="users/:username">ShowGithubUser</Link>
+        </li>
+      </ul>
     </Routes>
   );
 }
